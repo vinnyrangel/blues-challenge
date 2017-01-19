164cc9cf6849701c67df10a90a04fdf21da73516
Vinicius Rangel <vinny.rangel@gmail.com>
2017-01-19 10:05:49 -0200

    implementando o controller de adicionar veículo

--- vehicles/controllers/new.controller.js
+++ vehicles/controllers/new.controller.js
@@ -5,12 +5,35 @@
         .module('vehicles')
         .controller('newController', newController);
 
-    newController.$inject = ['storageService', 'usSpinnerService', 'vehiclesService'];
+    newController.$inject = ['storageService', 'usSpinnerService', 'vehiclesService', '$state'];
 
-    function newController(storageService, usSpinnerService, vehiclesService) {
+    function newController(storageService, usSpinnerService, vehiclesService, $state) {
         var vm = this;
 
-        
+        // Propriedades 
+        vm.veiculo = {
+            placa      : "",
+            marca      : "",
+            modelo     : "",
+            combustivel: "",
+            valor      : "",
+            foto       : ""
+        }       
+
+        // Métodos
+        vm.voltar = voltar;
+        vm.novoVeiculo = novoVeiculo;
+
+        /////////////////
+
+        function voltar() {
+            $state.go('main.vehicles.list');
+        }
+
+        function novoVeiculo(veiculo) {
+            vehiclesService.add(veiculo);
+            $state.go('main.vehicles.list');
+        }
         
     };
 })();
