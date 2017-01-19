5a981ae869a4bce31b7e91a67166140285ee43f0
Vinicius Rangel <vinny.rangel@gmail.com>
2017-01-19 09:22:39 -0200

    inserindo a carga inicial de veículos no serviço e iniciando a implementação dos métodos de adicionar e remover

--- vehicles/services/vehicles.service.js
+++ vehicles/services/vehicles.service.js
@@ -5,20 +5,32 @@
         .module('vehicles')
         .service('vehiclesService', vehiclesService);
 
-    vehiclesService.inject = [];
+    vehiclesService.$inject = [];
     function vehiclesService() {
+
+        // Propriedades
+        this.vehicles = [
+            { "combustivel" : "Flex", "imagem" : null, "marca" : "Volkswagem", "modelo" : "Gol", "placa" : "FFF-5498", "valor" : "20000"},
+
+            { "combustivel" : "Gasolina", "imagem" : null, "marca" : "Volkswagem", "modelo" : "Fox", "placa" : "FOX-4125", "valor" : "20000"},
+
+            { "combustivel" : "Alcool", "imagem" : "http://carros.ig.com.br/fotos/2010/290_193/Fusca2_290_193.jpg", "marca" : "Volkswagen", "modelo" : "Fusca", "placa" : "PAI-4121", "valor" : "20000"}
+        ];
+        this.selected = [];
+
+        // Métodos
         this.add = add;
         this.remove = remove;
 
         ////////////////
 
-        function add(vehicle) { 
-            
+        function add(vehicle) {
+            this.vehicles.push(vehicle);
         }
         
 
-        function remove(index) { 
-
+        function remove(index) {
+            this.vehicles.splice(index, 1);
         }
         
     }
