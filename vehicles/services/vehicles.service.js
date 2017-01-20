a944e53f0465385ff6f9f3aead4ee0ca81abb7c5
Vinicius Rangel <vinny.rangel@gmail.com>
2017-01-20 07:07:14 -0200

    adicionando mais veículos para testar a paginação

--- vehicles/services/vehicles.service.js
+++ vehicles/services/vehicles.service.js
@@ -14,7 +14,22 @@
 
             { "combustivel" : "Gasolina", "imagem" : null, "marca" : "Volkswagem", "modelo" : "Fox", "placa" : "FOX-4125", "valor" : "20000"},
 
-            { "combustivel" : "Alcool", "imagem" : "http://carros.ig.com.br/fotos/2010/290_193/Fusca2_290_193.jpg", "marca" : "Volkswagen", "modelo" : "Fusca", "placa" : "PAI-4121", "valor" : "20000"}
+            { "combustivel" : "Alcool", "imagem" : "http://carros.ig.com.br/fotos/2010/290_193/Fusca2_290_193.jpg", "marca" : "Volkswagen", "modelo" : "Fusca", "placa" : "PAI-4121", "valor" : "20000"},
+
+
+
+
+            { "combustivel" : "Flex", "imagem" : null, "marca" : "Honda", "modelo" : "Civic", "placa" : "FFF-5498", "valor" : "80000"},
+
+            { "combustivel" : "Alcool", "imagem" : null, "marca" : "Peugeot", "modelo" : "206", "placa" : "FOX-4125", "valor" : "20000"},
+
+            { "combustivel" : "Alcool", "imagem" : "http://carros.ig.com.br/fotos/2010/290_193/Fusca2_290_193.jpg", "marca" : "Ford", "modelo" : "Fiesta", "placa" : "PAI-4121", "valor" : "20000"},
+
+            { "combustivel" : "Gasolina", "imagem" : null, "marca" : "Volkswagem", "modelo" : "Polo", "placa" : "FFF-5498", "valor" : "20000"},
+
+            { "combustivel" : "Gasolina", "imagem" : null, "marca" : "Nissan", "modelo" : "Sentra", "placa" : "FOX-4125", "valor" : "20000"},
+
+            { "combustivel" : "Flex", "imagem" : "http://carros.ig.com.br/fotos/2010/290_193/Fusca2_290_193.jpg", "marca" : "BMW", "modelo" : "320i", "placa" : "PAI-4121", "valor" : "20000"}
         ];
         this.selected = [];
 
