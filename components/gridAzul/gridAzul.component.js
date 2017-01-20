9b9546badac584781e6a078288e994563c76900c
Vinicius Rangel <vinny.rangel@gmail.com>
2017-01-20 08:35:07 -0200

    implementando o método de carregamento de página

--- components/gridAzul/gridAzul.component.js
+++ components/gridAzul/gridAzul.component.js
@@ -24,17 +24,31 @@
     function gridAzulController() {
         var vm = this;
 
+        // Propriedades
         vm.paginaAtual = 1;        
+        vm.dadosPagina = []; // controla quais dados serão exibidos na página atual
+
+        // Métodos
         vm.carregarPagina = carregarPagina;
         
         ////////////////
 
         function carregarPagina(event) {
             console.log("Hora de carregar a página" + event.pagina);
+
+            // calculo onde iniciam os registros da próxima página
+            var inicio = (parseInt(event.pagina)-1) * vm.itensPorPagina;
+            console.log(inicio, "inicio");
+
+            var auxDados = angular.copy(vm.dados);
+            vm.dadosPagina = auxDados.splice(inicio, vm.itensPorPagina); 
+            console.log(vm.dadosPagina, "vm.dadosPagina");
+            vm.paginaAtual = event.pagina;
         }
 
         vm.$onInit = function() {
             console.log(vm.itensPorPagina, "itensPorPagina 1");
+            carregarPagina({pagina: 1});
         }
     }
 })();
